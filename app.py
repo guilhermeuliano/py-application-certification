3b5a8d021f6e666dcd1981ebffb3bf1be6aa5300
guilhermeuliano <guilherme_uliano@hotmail.com>
2024-02-17 17:04:55 -0300

    added comments app.py

--- app.py
+++ app.py
@@ -2,26 +2,32 @@ import openpyxl
 from PIL import Image, ImageDraw, ImageFont
 
 
-# Abrindo a planinha no python
+# Lendo a planinha no python
 planilha_alunos = openpyxl.load_workbook('planilha_alunos.xlsx')
+# Escolhendo a página que os dados estão na planilha
 pagina_planilha = planilha_alunos['Planilha1']
 
+# Criado um indice para cada id de aluno da planilha por linha
+# Como a primeira linha da planilha é um cabeçalho, min_row=2 é para começar a ler a partir da segunda linha, porém linha 2 = indice 0
 for indice, linha in enumerate(pagina_planilha.iter_rows(min_row=2)):
+
     # Pegando os dados da planilha
     curso_planilha = linha[0].value # Coluna do nome do curso
     participante_planilha = linha[1].value # Coluna do nome do aluno
     data_ini = linha[2].value # Coluna data inicio
-    data_ini_str = data_ini.strftime("%Y-%m-%d") # Conversão date para string
+    data_ini_str = data_ini.strftime("%Y-%m-%d") # Conversão da data inicial para string
     data_fim = linha[3].value # Coluna data final
-    data_fim_str = data_fim.strftime("%Y-%m-%d")
+    data_fim_str = data_fim.strftime("%Y-%m-%d") # Conversão da data final para string
     data_emi = linha[4].value # Coluna data emissão
-    data_emi_str = data_emi.strftime("%Y-%m-%d")
+    data_emi_str = data_emi.strftime("%Y-%m-%d") # Conversão da data emissao para string
 
-    #
+    # Duas fontes adicionadas para esse projeto
+    # font Rasa-Regular para todos textos com um tamanho de 48
     font_geral = ImageFont.truetype('./Rasa-Regular.ttf', 48)
+    # font Calligrapher só para o nome do aluno com um tamanho de 150
     font_nome = ImageFont.truetype('./Calligrapher.ttf', 150)
 
-    #
+    # Pegando a imagem do certificado sem informção na pasta
     image = Image.open('./certificado.png')
     desenhar = ImageDraw.Draw(image)
 
@@ -38,7 +44,6 @@ for indice, linha in enumerate(pagina_planilha.iter_rows(min_row=2)):
     desenhar.text((705,759),data_fim_str,fill='black',font=font_geral)
     desenhar.text((970,886),data_emi_str,fill='black',font=font_geral)
 
-
-
+    # local do save dos certificados com indice "id" e nome do participante
     image.save(f'./certificados/{indice} {participante_planilha}certificado.png')
 
